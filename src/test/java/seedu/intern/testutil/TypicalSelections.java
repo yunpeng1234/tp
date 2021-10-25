15b8d4f0566bc07e525c8ea7e70fe7c9d1dd7b69
Chang-CH <chang.chuanhao@dhs.sg>
2021-10-25 17:50:13 +0800

    Resolve conflicts

--- src/test/java/seedu/intern/testutil/TypicalSelections.java
+++ src/test/java/seedu/intern/testutil/TypicalSelections.java
@@ -10,8 +10,5 @@ public class TypicalSelections {
     public static final Selection SELECTION_FIRST_PERSON = Selection.fromIndex(INDEX_FIRST_PERSON);
     public static final Selection SELECTION_SECOND_PERSON = Selection.fromIndex(INDEX_SECOND_PERSON);
     public static final Selection SELECTION_THIRD_PERSON = Selection.fromIndex(INDEX_THIRD_PERSON);
-    public static final Pair<Index, Boolean> PAIR_FIRST_PERSON_TOGGLE = new Pair<>(INDEX_FIRST_PERSON, true);
-    public static final Pair<Index, Boolean> PAIR_SECOND_PERSON_TOGGLE = new Pair<>(INDEX_SECOND_PERSON, true);
-    public static final Pair<Index, Boolean> PAIR_THIRD_PERSON_TOGGLE = new Pair<>(INDEX_THIRD_PERSON, true);
     public static final Selection SELECTION_ALL = Selection.fromAllFlag();
 }
