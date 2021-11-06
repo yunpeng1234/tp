f54664acf6d9d7b3418fef59c3d092b0e18703a0
yunpeng1234 <xiao.yunpeng99@gmail.com>
2021-11-06 22:43:50 +0800

    Fix delete and edit error messages

--- src/main/java/seedu/intern/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/intern/logic/commands/DeleteCommand.java
@@ -18,8 +18,9 @@ public class DeleteCommand extends Command {
     public static final String COMMAND_WORD = "delete";
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the applicant identified by the index number used in the displayed applicant list.\n"
-            + "Parameters: INDEX (must be a positive integer)\n"
-            + "Example: " + COMMAND_WORD + " 1";
+            + "Parameters: INDEX (must be a positive integer) or ALL\n"
+            + "Example: " + COMMAND_WORD + " 1 or"
+            + COMMAND_WORD + "ALL";
 
     public static final String MESSAGE_DELETE_APPLICANT_SUCCESS = "Deleted applicant: %1$s";
     public static final String MESSAGE_DELETE_ALL_SUCCESS = " %1$s applicants deleted!";
